2607d2a08a7341fe4c05de039dae6c48d00e667f
hannapodeszwa <hanna.podeszwa@gmail.com>
2023-07-27 20:55:29 +0200

    Dodaj pliki projektów.

---
+++ Lab5/Data/Lab5Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Lab5.Models;
+
+namespace Lab5.Data
+{
+    public class Lab5Context : DbContext
+    {
+        public Lab5Context (DbContextOptions<Lab5Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Lab5.Models.Book> Book { get; set; } = default!;
+    }
+}
---
+++ Lab5/Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab5.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public string? Rating { get; set; }
+
+        //[DataType(DataType.Date)]
+        //public DateTime ReleaseDate { get; set; }
+        //public string Genre { get; set; }
+        //public decimal Price { get; set; }
+    }
+}
